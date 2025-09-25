1ed45ca60306b5cf99839570f7477fddc9204dfc
Tungston-labs <support@tungstonlabs.com>
2025-09-25 16:27:30 +0530

    change pwd,logout

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import AdminLoginView,ForgotPasswordView,VerifyOTPView,ResetPasswordView
+from .views import AdminLoginView,ForgotPasswordView,VerifyOTPView,ResetPasswordView,ChangePasswordView,LogoutView
 
 urlpatterns = [
     path('login/', AdminLoginView.as_view(), name='admin-login'),
     path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
     path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
     path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -8,6 +8,9 @@ from random import randint
 from .models import User, PasswordResetOTP
 from .serializers import ForgotPasswordSerializer, VerifyOTPSerializer, ResetPasswordSerializer
 from django.conf import settings
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import update_session_auth_hash
+
 
 class AdminLoginView(APIView):
     def post(self, request):
@@ -93,3 +96,40 @@ class ResetPasswordView(APIView):
         user.save()
         
         return Response({"message": "Password updated successfully"}, status=status.HTTP_200_OK)
+
+
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        current_password = request.data.get("current_password")
+        new_password = request.data.get("new_password")
+        confirm_new_password = request.data.get("confirm_new_password")
+
+        if not user.check_password(current_password):
+            return Response({"error": "Current password is incorrect."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if new_password != confirm_new_password:
+            return Response({"error": "New passwords do not match."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if current_password == new_password:
+            return Response({"error": "New password cannot be same as current password."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+        update_session_auth_hash(request, user)
+
+        return Response({"message": "Password updated successfully."}, status=status.HTTP_200_OK)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data.get("refresh")
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logout successful."}, status=status.HTTP_200_OK)
+        except Exception:
+            return Response({"error": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)
