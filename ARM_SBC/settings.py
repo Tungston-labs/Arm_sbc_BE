2623268b6ee0041c5ffd3523c16d039f91780284
Tungston-labs <support@tungstonlabs.com>
2025-09-26 12:19:29 +0530

    corsheader enabled

--- ARM_SBC/settings.py
+++ ARM_SBC/settings.py
@@ -63,11 +63,13 @@ INSTALLED_APPS = [
     'inquiries',
     'review',
     'shared',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
