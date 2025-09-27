f20219c027e2bf586305d3ea571993d780c9bdbd
Tungston-labs <support@tungstonlabs.com>
2025-09-27 09:42:07 +0530

    media root and urls

--- ARM_SBC/settings.py
+++ ARM_SBC/settings.py
@@ -143,8 +143,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-MEDIA_URL = '/product_images/'
-MEDIA_ROOT = BASE_DIR / 'product_images'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
